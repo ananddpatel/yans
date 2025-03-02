b9eb24ebd439cb03cd58570b66eb009e7675805a
Anand Patel <anandpatel@macbook.home>
2025-03-01 21:34:38 -0500

    fix

--- knexfile.ts
+++ knexfile.ts
@@ -1,25 +1,33 @@
-import type { Knex } from 'knex';
-import { resolve } from 'path';
+import type { Knex } from "knex";
+import { resolve } from "path";
 
 // Update with your config settings.
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: resolve(__dirname, './db-dev.sqlite3'),
+      filename: resolve(__dirname, "./db-dev.sqlite3"),
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
     },
   },
-
-  production: {
-    client: 'sqlite3',
+  test: {
+    client: "sqlite3",
     connection: {
-      filename: resolve(__dirname, './db-prod.sqlite3'),
+      filename: resolve(__dirname, "./db-test.sqlite3"),
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: resolve(__dirname, "./db-prod.sqlite3"),
+    },
+    migrations: {
+      tableName: "knex_migrations",
     },
   },
 };
